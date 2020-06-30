ace16fc671588ea9ea29f7d3533075ca58018f42
Tony <tony.inuma@gmail.com>
2020-06-30 18:51:18 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
