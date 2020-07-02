80de86436da97864532762ccf7226a349e43fbdb
Tony <tony.inuma@gmail.com>
2020-07-01 21:51:03 -0500

    Add: Validations fields with express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const { User } = require('../../db/db');
 
-router.post('/signin', async(req, res) => {
+router.post('/signin', [
+    check('username','username is required').not().isEmpty(),
+    check('email','email is incorrect').isEmail(),
+    check('password','pasword is required').not().isEmpty(),
+], async(req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errores: errors.array() });
+    }
 
     req.body.password = bcrypt.hashSync(req.body.password, 10)
     const user = await User.create(req.body);
     res.json(user);
+
 });
 
 
